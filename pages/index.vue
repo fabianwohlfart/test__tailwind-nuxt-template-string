a4130dcbafee70919d963edec40d35f6a22c9101
Fabian Wohlfart <mail@fabianwohlfart.com>
2021-02-08 09:30:50 +0100

    init: dynamic classes

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div
+      v-for="item in array"
+      :class="`ml-${ item }`"
+      >
+      Test
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      array: [1, 2, 3, 4, 5]
+    }
+  }
+}
+</script>
